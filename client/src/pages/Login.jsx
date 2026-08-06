30ec127f13419c8ab2a70305ad85bf5c67e22b7a
Ankush kushwaha <ankushkushwaha2829@gmail.com>
2026-08-06 10:47:19 +0530

    create the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:5000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      if (data.success) {
+        //saving the token
+        localStorage.setItem("token", data.token);
+        //saved logged in user
+        localStorage.setItem("user", JSON.stringify(data.user));
+        toast.success("login successful");
+        navigate("/");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error("Soemthing went wrong");
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white shadow-lg rounded-xl p-8 w-full max-w-md">
+        <h1 className="text-3xl font-bold text-center mb-8">Login</h1>
+
+        <form className="space-y-5" onSubmit={handleSubmit}>
+          <div>
+            <label className="block mb-2 font-medium">Email</label>
+
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full border rounded-lg p-3"
+              placeholder="Enter email"
+            />
+          </div>
+
+          <div>
+            <label className="block mb-2 font-medium">Password</label>
+
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="w-full border rounded-lg p-3"
+              placeholder="Enter password"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-black text-white py-3 rounded-lg hover:bg-gray-800"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -44,6 +44,7 @@ const registerUser = async (req, res) => {
 //login user...
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
+  console.log(email , password) ; 
   try {
     if (!email || !password) {
       return res.status(400).json({
